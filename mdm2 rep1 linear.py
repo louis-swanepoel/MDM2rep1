f53f496dc007dcf546994a267dfc0599670ae5e0
ARudesill <98823317+ARudesill@users.noreply.github.com>
2022-12-05 05:19:14 +0000

    both image creation included

--- mdm2 rep1 linear.py
+++ mdm2 rep1 linear.py
@@ -6,13 +6,13 @@ Created on Mon Nov 14 19:55:03 2022
 """
 import math
 from scipy.optimize import fsolve
-from PIL import Image
+from PIL import Image#, ImageDraw
 import numpy as np
 from pathlib import Path
+from matplotlib import pyplot as plt
 
-
-
-data_folder = Path("C:/Users/ashru/Desktop")
+#Change data folder to wherever on your 
+data_folder = "C:/Users/ashru/Desktop"
 
 #define a function for printing the matrix
 def printArea():
@@ -21,80 +21,11 @@ def printArea():
             print(area[i][j], end = " ")
             print("\n")
         print("\n")
-    
-def getAreaLayout():
-    try:
-        #ask for the dimensions of the area
-        areaWidth = int(input('Please input the width of the area \n'))
-        areaHeight = int(input('Please input the height of the area \n'))
-        print("\n")
-    except:
-        print("Something you entered was not an integer. Please try again. \n")
-        areaWidth, areaHeight = getAreaLayout()
-    if(areaWidth < 1):
-        print("Area width must be greater than zero. Please try again. \n")
-        areaWidth, areaHeight = getAreaLayout()
-    if(areaHeight < 1):
-        print("Area height must be greater than zero. Please try again. \n")
-        areaWidth, areaHeight = getAreaLayout()
-    return areaWidth, areaHeight
+        
 
-def getAreaDist():
-    try:
-        dist = float(input("Please input the distance between each measuring point in the area (m) \n"))
-    except:
-        print("The distance must be a float. Please try again. \n")
-        dist = getAreaDist()
-    if(dist<=0):
-        print("The distance must be greater than zero. Please try again. \n")
-        dist = getAreaDist()
-    return dist
+        
 
-
-#define a function for getting info for the time information
-def getTime():
-    try:
-        #ask for time peroid and step
-        timePeriod = float(input("what is the time period you would like to measure over (s) \n"))
-        timeStep = float(input("what is the time step you would like to use (s) \n"))
-    except:
-        #if invalid input, send error message and re-call function
-        print("Something you entered was not a float. Please try again.\n")
-        timePeriod, timeStep = getTime()
-    #if invalid input, send error message and re-call function
-    if(timePeriod<=0):
-        print("You cannot have a time period less than or equal to 0. Please try again. \n")
-        timePeriod, timeStep = getTime()
-    elif(timePeriod<timeStep):
-        print("you cannot have a time period less than your time step. Please try again. \n")
-        timePeriod, timeStep = getTime()
-    elif(timeStep<=0):
-        print("You cannot have a time step less than or equal to 0. Please try again. \n")
-        timePeriod, timeStep = getTime()
-    return timePeriod, timeStep
-
-def getFrequency():
-    try:
-       f = float(input("What is the frequency of the sound wave? \n")) 
-    except:
-        print("You did not enter a float. Please try again. \n")
-        f =getFrequency()
-    if(f<=0):
-        print("Frequency must be over 0. Please try again. \n")
-        f = getFrequency()
-    return f
-
-def getXY():
-    try:
-        xEM = float(input("What would you like the slope of x to equal (in terms of t)? \n"))
-        xEB = float(input("What would you like the starting value of x to equal? \n"))
-        yEM = float(input("What would you like the slope of y to equal (in terms of t)? \n"))
-        yEB = float(input("What would you like the starting value of y to equal? \n"))
-    except:
-        print("You must enter a float. Please try again. \n")
-        xEM, xEB,yEM,yEB = getXY()
-    return xEM, xEB, yEM, yEB
-
+SPEEDOFSOUND = 343.2
 
 #define variables for x measured, y measured 
 xM = int()
@@ -102,23 +33,41 @@ yM = int()
 
 #define a variable for time emitted, time measured, and retarded time
 tE = float()
-tEList = []
 tMList = []
 tR = float() 
 tM= 0
 pressure = []
 
+#define speed of sound
+SPEEDOFSOUND = 343.2
 
-timePeriod, timeStep = getTime()   
-f = getFrequency()
+""" Beginning of inputs """
+
+#define timePeriod and timeStep
+timePeriod, timeStep = 1,1
+#define the frequency of the sound source
+f = 50
+#calculate the number of timeSteps the script will go through
 stepNumber = math.floor(timePeriod/timeStep)
+print(stepNumber)
+
+#define the height and width of the continuous model picture in pixels (wavefront will be double the size)
+areaWidth, areaHeight = 400, 400
+
+#define the distance between each pixel in the image
+dist = 2
+#define the x and y equation m & b (in the form x=xEM*t+xEB etc)
+xEM, xEB, yEM, yEB = 0, -50, 0,-50
+
+
+""" End of inputs """
 
 #create an empty 2d matrix with given dimensions 
 
-areaWidth, areaHeight = getAreaLayout()
-area = [0] *(stepNumber+1)
+areaWidth, areaHeight = 400, 400
+area = [0] *(stepNumber)
 #areaBW = [0] *(stepNumber+1)
-for i in range(stepNumber+1):
+for i in range(stepNumber):
     area[i] = [0] * areaHeight
     #areaBW[i] = [0] * areaHeight
     for j in range(areaHeight):
@@ -129,65 +78,112 @@ areaBW = np.zeros((areaHeight,areaWidth))
 
 
 
-dist = getAreaDist()
-xEM,xEB,yEM,yEB = getXY()
 
+
+totalWidthDistHalf = dist*areaWidth/2
+totalHeightDistHalf = dist*areaHeight/2
 
 w= 2*math.pi*f
 
 
+
+tEList=[]
+
+plt.rcParams["figure.figsize"] = [areaWidth/100, areaHeight/100]
+plt.rcParams["figure.autolayout"] = True
+
+plt.title("Wavefront Diagram of a Single Point Sound Source with Equations: \n x="+str(xEM)+"t+"+str(xEB)+", y="+str(yEM)+"t+"+str(yEB)+"\n at Time: t="+str(round(timeStep*(stepNumber),3)))
+plt.xlabel("x (m)") 
+plt.ylabel("y (m)") 
+plt.axis([-totalWidthDistHalf, totalWidthDistHalf, -totalHeightDistHalf, totalHeightDistHalf])
+plt.axis('square')
+plt.gcf().set_size_inches(areaWidth/50, areaWidth/50)
+
+
+
+numCircles = math.floor((stepNumber)*f*timeStep)
+circlesList = []
+for i in range(numCircles):
+    circTime = i/f
+    circlesList.append([circTime,circTime*xEM+xEB,circTime*yEM+yEB])
+    c = plt.Circle((circTime*xEM+xEB,circTime*yEM+yEB), radius =(SPEEDOFSOUND*(timeStep*stepNumber-circTime)), fill = False)
+    plt.gca().add_artist(c)
+print(circlesList)
+imgData = str(timePeriod) + ',' + str(timeStep) + ',' + str(f) + ',' + str(areaHeight) + ',' + str(areaWidth) + ',' + str(dist) + ',' +  str(xEM) + ',' + str(xEB) + ',' + str(yEM) + ',' + str(yEB)
+dataFolder = Path("C:/Users/ashru/Desktop/"+imgData)
+p = dataFolder.mkdir(parents=True, exist_ok=True)
+plt.savefig("C:/Users/ashru/Desktop/"+imgData+ '/'+imgData +",Wavefront.png",dpi=100)
+plt.show()
+
+yEM, yEB= yEM * -1, yEB * -1
+speedEst = yEM+xEM
+
+
 for j in range(len(area[0])):
-    yM = j * dist
+    yM = j * dist - totalHeightDistHalf
     for k in range(len(area[0][0])):  
-        xM = k * dist
-        for i in range(stepNumber + 1):
+        xM = k * dist - totalWidthDistHalf
+        for i in range(stepNumber):
             #set xM and yM based on current place measured * dist
-            tM = i * timeStep
+            tM = (i+1) * timeStep 
             tMList.append(tM)
             def equations(p):
                 x, y = p
-                return (x+y-tM, x-((((xEM*y+xEB)-xM)**2+((yEM*y+yEB)-yM)**2)**.5)/340)
+                return (x+y-tM, x-((((xEM*y+xEB)-xM)**2+((yEM*y+yEB)-yM)**2)**.5)/340,)
                 #return (x+y-tM, x-((((xEM*y+xEB)-xM)**2+((yEM*y+yEB)-yM)**2)**.5)/340)
-            tR, tE=  fsolve(equations, ((i*timeStep/2), (i*timeStep/2)))
-            if(tE>=0):
-                tEList.append(tE)
-                #set r based on formula for x=t, y=t, measured fro (0,0)
-                r = ((tE-xM)**2+(tE-yM)**2)**.5
-                #x = setX(tE)
-                #y = setY(tE)
-                #create a variable for the farthest 
-                """
-                if(r==0):
+            """
+            if(speedEst>=SPEEDOFSOUND):
+                tETotal = 0
+                tR, tE=  fsolve(equations, (0, 0))
+                if (tE>=0 and tE<= tM):
+                    if(tE in tEList==False):
+                        tEList.append(tE)
+                        tETotal+=tE
+                tR, tE=  fsolve(equations, (stepNumber*timeStep, stepNumber*timeStep))
+                if (tE>=0 and tE<= tM):
+                    if(tE in tEList==False):
+                        tEList.append(tE)
+                        tETotal+=tE
+                area[i][j][k] = (math.sin(w*tETotal))
+                tEList = []
+            else:
+                tR, tE=  fsolve(equations, ((tM/2), (tM/2)))
+                if (tE>=0 and tE<= tM):
                     area[i][j][k] = (math.sin(w*tE))
-                else:
-                    area[i][j][k] = (math.sin(w*tE)/(r**2))
-                """
+            """
+            tR, tE=  fsolve(equations, ((tM/2), (tM/2)))
+            if (tE>=0 and tE<= tM):
                 area[i][j][k] = (math.sin(w*tE))
+print('equations done')
 
 
 
-printArea()
-images= []
 
-yN = input("Would you like to make any of the time slots into an image (enter y for yes).")
-if(yN=='y'):
-    #step = int(input("Which step would you like to use (start at 1)"))-1
-    for k in range(len(area)):
-        for i in range(len(area[k])):
-            for j in range(len(area[k][i])):
-                bV = area[k][i][j]
-                if(bV>1):
-                    bv=1
-                elif(bV<-1):
-                    bV = -1
-                areaBW[i][j] = int(round(127.5*(bV+1)))
-        areaBW = areaBW.astype('i')
-        print(areaBW)
-        newImg = Image.fromarray(np.uint8(areaBW))
-        name = "C:/Users/ashru/Desktop/newImg"+str(k)+".png"
-        newImg.save(name)
-        #images.append(newImg)
-#images2gif.writeGif("C:/Users/ashru/Desktop/images.gif",images,duration=0.3,dither=0)
-#images[0].save('images.gif',save_all=True, append_images=images[1:], optimize=False, duration=40, loop=0)
-#print(images)
-    
+#yN = input("Would you like to make any of the time slots into an image (enter y for yes).")
+#if(yN=='y'):im
+#step = int(input("Which step would you like to use (start at 1)"))-1
+for k in range(len(area)):
+    for i in range(len(area[k])):
+        for j in range(len(area[k][i])):
+            bV = area[k][i][j]
+            if(bV>1):
+                bv=1
+            elif(bV<-1):
+                bV = -1
+            areaBW[i][j] = int(round(127.5*(bV+1)))
+    areaBW = areaBW.astype('i')
+    newImg = Image.fromarray(np.uint8(areaBW))
+
+    name = "C:/Users/ashru/Desktop/"+imgData+ '/'+ imgData + ',' + str(k) +".png"
+    #name1 = "C:/Users/ashru/Desktop/"+imgData+ '/Circle'+ imgData + ',' + str(k) +".png"
+    newImg.save(name)
+f = open("C:/Users/ashru/Desktop/"+imgData+ '/'+ imgData + ',' + str(k) +".txt",'w')
+write1 = "x = "+str(xEM)+"t + "+str(xEB)
+write2 = "y = "+str(yEM)+"t + "+str(yEB)
+write3 = "time step "+str(timeStep)+", time measured "+str(timePeriod)
+write4 = "frequency "+str(f)
+f.write(write1)
+f.write(write2)
+f.write(write3)
+f.write(write4)
+print('done')
